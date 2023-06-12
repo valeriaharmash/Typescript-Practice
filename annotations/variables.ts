cd70a9b209189e6d265a4c089cd7f84045e0cb95
Valeriia Harmash <valeriaharmash@gmail.com>
2023-06-12 15:05:03 -0400

    type annotations samples

---
+++ annotations/variables.ts
@@ -0,0 +1,30 @@
+//variable with type annotation
+const apples: number = 5;
+let nothing: undefined = undefined;
+let someString: string = 'abc';
+let nothingMuch: null = null;
+
+//built in Objects
+
+let now: Date = new Date();
+
+//Array
+
+let colors: string[] = ['blue', 'yellow'];
+let muNums: number[] = [1,2,3];
+let truths: boolean[] = [true, false, true];
+
+//Classes
+
+class Car {
+
+}
+
+let car: Car = new Car();
+
+//object literal
+
+let point: {x: number; y: number } = {
+	x: 10,
+	y: 9
+}
